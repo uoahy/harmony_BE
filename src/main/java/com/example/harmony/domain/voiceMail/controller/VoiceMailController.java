e593dcd4b87e67e6d733444d313b172089b62034
lemon203213 <lemon203213@gmail.com>
2022-08-13 15:18:13 +0900

    소리샘 생성 기능

--- src/main/java/com/example/harmony/domain/voiceMail/controller/VoiceMailController.java
+++ src/main/java/com/example/harmony/domain/voiceMail/controller/VoiceMailController.java
@@ -5,7 +5,6 @@ import com.example.harmony.domain.voiceMail.dto.AllVoiceMailsResponse;
 import com.example.harmony.domain.voiceMail.dto.VoiceMailRequest;
 import com.example.harmony.domain.voiceMail.service.VoiceMailService;
 import com.example.harmony.global.common.SuccessResponse;
-import com.example.harmony.global.s3.S3Service;
 import com.example.harmony.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,31 +13,38 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @RestController
 public class VoiceMailController {
 
     private final VoiceMailService voiceMailService;
-    private final S3Service s3Service;
+
 
     @GetMapping("/api/voice-mails")//목록
     public ResponseEntity<SuccessResponse> allVoiceMails(@AuthenticationPrincipal User user){
+
         AllVoiceMailsResponse allVoiceMailsResponse = voiceMailService.allVoiceMails(user);
         return new ResponseEntity<>(new SuccessResponse(HttpStatus.OK,"소리샘 조회 성공", allVoiceMailsResponse), HttpStatus.OK);
     }
 
     @PostMapping("/api/voice-mails")//생성
-    public ResponseEntity<?> createVoiceMail(@AuthenticationPrincipal UserDetailsImpl userDetails,
+    public ResponseEntity<SuccessResponse> createVoiceMail(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                              @RequestBody VoiceMailRequest voiceMailRequest,
-                                             @RequestPart(value = "file", required = false) MultipartFile file){
-        voiceMailService.createVoiceMail(voiceMailRequest, userDetails.getUser());
-        soundUrl= s3Service.uploadFile(file);
+                                             @RequestPart(value = "file", required = false) MultipartFile soundFile){
+
+        User user = userDetails.getUser();
+        voiceMailService.createVoiceMail(voiceMailRequest, soundFile, user);
+
         return new ResponseEntity<>(new SuccessResponse(HttpStatus.CREATED,"소리샘 등록 성공"),HttpStatus.OK);
     }
 
     @DeleteMapping("/api/voice-mails/{voiceMailId}")//삭제
-    public ResponseEntity<SuccessResponse> deleteVoiceMail(@AuthenticationPrincipal User user,
-                                             @PathVariable Long voiceMailId){
+    public ResponseEntity<SuccessResponse> deleteVoiceMail(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                           @PathVariable Long voiceMailId,
+                                                           @RequestBody Map<String, String>){
+        User user = userDetails.getUser();
 
         return new ResponseEntity<>(new SuccessResponse(HttpStatus.OK,"소리샘 삭제 성공"),HttpStatus.OK);
     }
--- src/main/java/com/example/harmony/domain/voiceMail/service/VoiceMailService.java
+++ src/main/java/com/example/harmony/domain/voiceMail/service/VoiceMailService.java
@@ -1,16 +1,25 @@
 package com.example.harmony.domain.voiceMail.service;
 
+import com.example.harmony.domain.user.entity.Family;
 import com.example.harmony.domain.user.entity.User;
+import com.example.harmony.domain.user.repository.FamilyRepository;
 import com.example.harmony.domain.user.repository.UserRepository;
 import com.example.harmony.domain.voiceMail.dto.AllVoiceMailsResponse;
 import com.example.harmony.domain.voiceMail.dto.VoiceMailRequest;
 import com.example.harmony.domain.voiceMail.entity.VoiceMail;
 import com.example.harmony.domain.voiceMail.repository.VoiceMailRepository;
+import com.example.harmony.global.s3.S3Service;
+import com.example.harmony.global.s3.UploadResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -18,14 +27,29 @@ public class VoiceMailService {
 
     private final VoiceMailRepository voiceMailRepository;
     private final UserRepository userRepository;
+    private final FamilyRepository familyRepository;
+    private final S3Service s3Service;
 
     public AllVoiceMailsResponse allVoiceMails(User user){//소리샘 목록
 
         List<VoiceMail> VoiceMails=voiceMailRepository.FindAllByFamilyIdByCreatedAtDesc(user.getFamily().getId());
         return new AllVoiceMailsResponse((VoiceMail) VoiceMails);
     }
-    public ResponseEntity<?> createVoiceMail(VoiceMailRequest voiceMailRequest, User user){
+    @Transactional
+    public void createVoiceMail(VoiceMailRequest voiceMailRequest, MultipartFile soundFile, User user){
 
+        User userId = userRepository.findById(user.getId())
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"등록되지 않은 사용자입니다."));
+        Family familyId= familyRepository.findById(user.getFamily().getId())
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"등록되지 않은 가족입니다."));
+        UploadResponse sound= s3Service.uploadFile(soundFile);
+
+        VoiceMail voiceMail=voiceMailRepository.save(new VoiceMail(userId, familyId, voiceMailRequest, sound));
+        //확실치 않음
     }
+
+    @Transactional
+    public void deleteVoiceMail()
+
 
 }
