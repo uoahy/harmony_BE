d2aedac57da8c9f99289824534ad148364e0540a
KimHyeon <hye07on11@gmail.com>
2022-09-01 04:50:46 +0900

    :bug: 갤러리 이미지 삭제 기능 버그 해결
    - @ResponseBody 어노테이션은 기본 생성자가 필요하기때문에 @NoArgsConstructor 어노테이션 추가
    - @Builder 어노테이션을 사용하고있기때문에 @AllArgsConstructor 어노테이션도 추가

--- src/main/java/com/example/harmony/domain/gallery/dto/ImageRemoveRequest.java
+++ src/main/java/com/example/harmony/domain/gallery/dto/ImageRemoveRequest.java
@@ -1,13 +1,17 @@
 package com.example.harmony.domain.gallery.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Getter
 public class ImageRemoveRequest {
 
-    List<Long> imageIds;
+    private List<Long> imageIds;
 }
