ccee701858bdcca57b70dc41de2f7fcaabd13661
lemon203213 <lemon203213@gmail.com>
2022-09-08 03:14:51 +0900

    :sparkles: Ranking 점수 중복 시 랭킹 설정 해결 & api 수정에 따른 변경 사항

--- src/main/java/com/example/harmony/domain/ranking/service/RankingService.java
+++ src/main/java/com/example/harmony/domain/ranking/service/RankingService.java
@@ -24,9 +24,7 @@ public class RankingService {
 
     private final FamilyRepository familyRepository;
 
-
     @Scheduled(cron = "* * 6 * * MON")
-//6시에 월요일마다
     int RankingMethod(int rk, Long fId) {
 
         List<Family> familyList = familyRepository.findAll(Sort.by(Sort.Direction.ASC, "monthlyScore"));
@@ -37,52 +35,8 @@ public class RankingService {
         return rk;
     }
 
-    @Scheduled(cron = "* * 6 * * MON")
-    List top10List() {
-        List<Family> familyList = familyRepository.findAll(Sort.by(Sort.Direction.ASC, "monthlyScore"));
-        List list = new ArrayList<>();
-        List fl = new ArrayList<>();
-        Map<String, Object> map = new HashMap<>();
-
-        for (Family familys : familyList) {
-            map.put("familyName", familys.getFamilyName());
-            map.put("score", familys.getMonthlyScore());
-            fl.add(map);
-        }
-
-        for (int i = 0; i < 10; i++) {
-            list.add(fl.get(i));
-
-        }
-        return list;
-    }
-
-
-    public RankingResponse getFamilyScore(User user) {
-        Family family = familyRepository.findById(user.getFamily().getId())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "가족 찾을 수가 없어"));
-        ;
-        Long familyId = user.getFamily().getId();
-
-        long familyCount = familyRepository.count();//총 가족 수
-        int top = (int) (familyCount * (1 / 10));//상위 10%에 속하는 가족수
-
-        int totalScore = family.getTotalScore();
-
+    int leveling(int totalScore) {
         int level;
-        int ranking = 0;
-
-
-        ranking = RankingMethod(ranking, familyId);
-
-
-        if (ranking < top) {
-            family.setFlower();
-        }
-
-        List topList = top10List();
-
-
         if (totalScore >= 3000) {
             level = 4;
         } else if (totalScore >= 2999 && totalScore <= 770) {
@@ -94,6 +48,63 @@ public class RankingService {
         } else {
             level = 0;
         }
+        return level;
+    }
+
+    @Scheduled(cron = "* * 6 * * MON")
+    List top10List() {
+        List<Family> familyList = familyRepository.findAll(Sort.by(Sort.Direction.ASC, "monthlyScore"));
+        List list = new ArrayList<>();
+        List fl = new ArrayList<>();
+        Map<String, Object> map = new HashMap<>();
+        int ranking = 1;
+
+        for (Family familys1 : familyList) {
+            map.put("ranking", ranking);
+            map.put("level", leveling(familys1.getTotalScore()));
+            map.put("familyName", familys1.getFamilyName());
+            map.put("score", familys1.getMonthlyScore());
+            fl.add(map);
+
+            List score = new ArrayList<>();
+            Map<String, Object> map1 = new HashMap<>();
+            for (Family familys2 : familyList) {
+                map1.put("score", familys2.getMonthlyScore());
+                score.add(map1);//점수 비교용 하나 생성
+
+                if (ranking != 1) {
+                    if (score.get(ranking - 2) != score.get(ranking--)) {//인덱스 기준
+                        ranking++;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            list.add(fl.get(i));
+        }
+        return list;
+    }
+
+
+    public RankingResponse getFamilyScore(User user) {
+        Family family = familyRepository.findById(user.getFamily().getId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "가족 찾을 수가 없어"));
+        Long familyId = user.getFamily().getId();
+
+        long familyCount = familyRepository.count();//총 가족 수
+        int top = (int) (familyCount * (1 / 10));//상위 10%에 속하는 가족수
+        int totalScore = family.getTotalScore();
+        int level;
+        int ranking = 0;
+
+        ranking = RankingMethod(ranking, familyId);
+        if (ranking < top) {
+            family.setFlower();
+        }
+
+        List topList = top10List();
+        level = leveling(totalScore);
 
         return new RankingResponse(family, ranking, level, topList);
     }
