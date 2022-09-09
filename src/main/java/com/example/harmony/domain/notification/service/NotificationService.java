3b1178486d36d06a409f918f051808b20edaaf77
KimHyeon <hye07on11@gmail.com>
2022-09-09 19:26:34 +0900

    :sparkles: 알림 생성 및 전송 기능 구현

--- src/main/java/com/example/harmony/domain/notification/service/NotificationService.java
+++ src/main/java/com/example/harmony/domain/notification/service/NotificationService.java
@@ -1,16 +1,20 @@
 package com.example.harmony.domain.notification.service;
 
 import com.example.harmony.domain.notification.model.Notification;
+import com.example.harmony.domain.notification.model.NotificationRequest;
+import com.example.harmony.domain.notification.model.NotificationResponse;
 import com.example.harmony.domain.notification.model.NotificationsListResponse;
 import com.example.harmony.domain.notification.repository.NotificationRepository;
 import com.example.harmony.domain.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -18,9 +22,20 @@ public class NotificationService {
 
     private final NotificationRepository notificationRepository;
 
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+
     public NotificationsListResponse getNotifications(User user) {
         List<Notification> notifications = notificationRepository.findAllByUserIdOrderByCreatedAtDesc(user.getId());
         return new NotificationsListResponse(notifications);
+    }
+
+    public void createNotification(NotificationRequest notificationRequest, List<User> user) {
+        List<Notification> notifications = notificationRepository.saveAll(user.stream()
+                .map(x -> new Notification(notificationRequest, x))
+                .collect(Collectors.toList()));
+        for (Notification notification : notifications) {
+            simpMessageSendingOperations.convertAndSend("/topic/user/" + notification.getUser().getNickname(), new NotificationResponse(notification));
+        }
     }
 
     @Transactional
