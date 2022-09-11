e558a7e73b6a187a94dd021102dcc53d4d5c35da
KimHyeon <hye07on11@gmail.com>
2022-09-11 21:55:10 +0900

    :white_check_mark: 리포지토리 단위테스트시 실제 DB와 연결

--- src/test/java/com/example/harmony/domain/schedule/repository/ScheduleRepositoryTest.java
+++ src/test/java/com/example/harmony/domain/schedule/repository/ScheduleRepositoryTest.java
@@ -5,6 +5,7 @@ import com.example.harmony.domain.user.model.Family;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
@@ -14,6 +15,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 class ScheduleRepositoryTest {
 
