0478546055888534bdc5b516bfbc5fbca52dfc8a
KimHyeon <hye07on11@gmail.com>
2022-09-12 00:18:52 +0900

    :white_check_mark: 리포지토리 단위테스트시 인메모리 DB와 연결

--- src/test/java/com/example/harmony/domain/gallery/repository/GalleryCommentRepositoryTest.java
+++ src/test/java/com/example/harmony/domain/gallery/repository/GalleryCommentRepositoryTest.java
@@ -5,13 +5,11 @@ import com.example.harmony.domain.gallery.entity.GalleryComment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 class GalleryCommentRepositoryTest {
 
